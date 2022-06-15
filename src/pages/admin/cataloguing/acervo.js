df1dc3e4d5cf8a37ec328587184d991d66fcf44b
Inácio Oliveira <inacioigne@gmail.com>
2022-06-15 17:00:52 -0400

    Import

--- src/pages/admin/cataloguing/acervo.js
+++ src/pages/admin/cataloguing/acervo.js
@@ -3,6 +3,7 @@ import Layout from "src/layouts/layoutMain";
 import { ThemeProvider } from "@mui/material/styles";
 import { DashboardLayout } from "src/layouts/dashboard-layout";
 import { theme } from "src/theme";
+import Link from 'next/link'
 import {
   Card,
   Box,
@@ -10,29 +11,80 @@ import {
   CardContent,
   Typography,
   Skeleton,
+  Button,
+  CardActions,
+  TextField,
+  IconButton
 } from "@mui/material/";
+import SearchIcon from '@mui/icons-material/Search';
+import { useForm, useFieldArray } from "react-hook-form";
+import { Controller } from "react-hook-form";
 
 export default function Acervo({ docs }) {
-  console.log("A: ", docs);
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      firstName: '',
+      select: {}
+    }
+  });
 
   return (
     <Container>
+      <Box sx={{display: "flex", m: 2, justifyContent: "center"}}>
+      {/* <TextField id="outlined-basic" label="Busca" size={"small"} variant="outlined" /> */}
+      <Controller
+        name="search"
+        control={control}
+        render={({ field }) => 
+        <TextField 
+        {...field}
+        label={"search"}
+        size={"small"}
+         />}
+      />
+      <IconButton aria-label="search" size="large">
+      <SearchIcon />
+
+      </IconButton>
+     
+
+      </Box>
+      
       {docs.map((doc, index) => (
         <Card key={index} sx={{ display: "flex", m: 3 }}>
           <Skeleton variant="rectangular" width={150} height={195} />
           <Box sx={{ display: "flex", flexDirection: "column" }}>
             <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography component="div"  variant="h6" >
-                {doc.title}
-              </Typography>
+              <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+                <Typography component="div" variant="h6">
+                  {doc.title}
+                </Typography>
+                <Typography component="div" variant="h6" sx={{ ml: 1 }}>
+                  {doc.subtitle}
+                </Typography>
+              </Box>
               <Typography
                 variant="subtitle1"
                 color="text.secondary"
                 component="div"
               >
-                {doc.author}
+                {doc.responsibilities}
               </Typography>
+              {doc.termo_topico.map((termo, index_termo) => (
+
+                <Button key={index_termo} variant="outlined" size="small" sx={{mr: 1}}>{termo}</Button>
+              ))}
+
+              
+
             </CardContent>
+            <CardActions>
+            <Link href={`/admin/cataloguing/${doc.id}`}>
+            <Button>Detalhes</Button>
+            </Link>
+            
+
+            </CardActions>
           </Box>
         </Card>
       ))}
